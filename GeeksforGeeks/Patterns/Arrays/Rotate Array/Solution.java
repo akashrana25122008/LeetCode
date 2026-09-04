990a8ab08c883029d012dc8cc3bd3651b82d3330
akashrana25122008 <akashrana25122008@gmail.com>
2026-09-04 14:25:41 +0530

    Add Medium Java Solution for Rotate Array from GeeksforGeeks

--- GeeksforGeeks/Patterns/Arrays/Rotate Array/Solution.java
+++ GeeksforGeeks/Patterns/Arrays/Rotate Array/Solution.java
@@ -1,21 +1,17 @@
 class Solution {
     public void rotateArr(int arr[], int d) {
-        int n = arr.length;
-        d = d % n;
-
-        reverse(arr, 0, d - 1);
-        reverse(arr, d, n - 1);
-        reverse(arr, 0, n - 1);
-    }
-
-    public void reverse(int arr[], int start, int end) {
-        while (start < end) {
-            int temp = arr[start];
-            arr[start] = arr[end];
-            arr[end] = temp;
-
-            start++;
-            end--;
+        // code here
+        d = d%arr.length;
+        int[] temp= new int[d];
+        for(int i = 0; i<d;i++){
+            temp[i] = arr[i];
+            
+        }
+        for(int i=d;i<arr.length;i++){
+            arr[i-d] = arr[i];
+        }
+        for(int i =arr.length-d;i<arr.length;i++){
+            arr[i]=temp[i-(arr.length-d)];
         }
     }
 }
